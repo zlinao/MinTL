0212ee4c86dde91c3dc4883c872cb62d1705a51b
Phi Nguyễn <phi.nguyen.uet@gmail.com>
2021-10-26 15:25:57 +0700

    update

--- T5.py
+++ T5.py
@@ -14,114 +14,112 @@ class MiniT5(MT5ForConditionalGeneration):
         decoder_config = deepcopy(config)
         decoder_config.is_decoder = True
 
-        self.dst_decoder = type(self.decoder)(decoder_config, self.shared)
-        self.dst_decoder.load_state_dict(self.decoder.state_dict())
-        self.dst_lm_head = type(self.lm_head)(config.d_model, config.vocab_size, bias=False)
-        self.dst_lm_head.load_state_dict(self.lm_head.state_dict())
+        self.decoder = type(self.decoder)(decoder_config, self.shared)
+        self.decoder.load_state_dict(self.decoder.state_dict())
+        self.lm_head = type(self.lm_head)(config.d_model, config.vocab_size, bias=False)
+        self.lm_head.load_state_dict(self.lm_head.state_dict())
 
     def tie_decoder(self):
         decoder_config = deepcopy(self.config)
         decoder_config.is_decoder = True
-        self.dst_decoder = type(self.decoder)(decoder_config, self.shared)
-        self.dst_decoder.load_state_dict(self.decoder.state_dict())
-        self.dst_lm_head = type(self.lm_head)(self.config.d_model, self.config.vocab_size, bias=False)
-        self.dst_lm_head.load_state_dict(self.lm_head.state_dict())
+        self.decoder = type(self.decoder)(decoder_config, self.shared)
+        self.decoder.load_state_dict(self.decoder.state_dict())
+        self.lm_head = type(self.lm_head)(self.config.d_model, self.config.vocab_size, bias=False)
+        self.lm_head.load_state_dict(self.lm_head.state_dict())
 
-    def forward(
-        self,
-        input_ids=None,
-        attention_mask=None,
-        decoder_input_ids=None,
-        decoder_attention_mask=None,
-        head_mask=None,
-        decoder_head_mask=None,
-        cross_attn_head_mask=None,
-        encoder_outputs=None,
-        past_key_values=None,
-        inputs_embeds=None,
-        decoder_inputs_embeds=None,
-        lm_labels=None,
-        use_cache=None,
-        output_attentions=None,
-        output_hidden_states=None,
-        return_dict=None,
-    ):
-        if decoder_input_ids[0, 0] == self.config.decoder_start_token_id:
-            decoder = self.dst_decoder
-            lm_head = self.dst_lm_head
-        else:
-            decoder = self.decoder
-            lm_head = self.lm_head
-
-        # Encode if needed (training, first prediction pass)
-        if encoder_outputs is None:
-            # Convert encoder inputs in embeddings if needed
-            encoder_outputs = self.encoder(
-                input_ids=input_ids,
-                attention_mask=attention_mask,
-                inputs_embeds=inputs_embeds,
-                head_mask=head_mask,
-                output_attentions=output_attentions,
-                output_hidden_states=output_hidden_states,
-                return_dict=return_dict,
-            )
-
-        elif return_dict and not isinstance(encoder_outputs, BaseModelOutput):
-            encoder_outputs = BaseModelOutput(
-                last_hidden_state=encoder_outputs[0],
-                hidden_states=encoder_outputs[1] if len(encoder_outputs) > 1 else None,
-                attentions=encoder_outputs[2] if len(encoder_outputs) > 2 else None,
-            )
-
-        hidden_states = encoder_outputs[0]
-
-        if lm_labels is not None and decoder_input_ids is None and decoder_inputs_embeds is None:
-            # get decoder inputs from shifting lm labels to the right
-            decoder_input_ids = self._shift_right(lm_labels)
-
-        # Decode
-        decoder_outputs = decoder(
-            input_ids=decoder_input_ids,
-            attention_mask=decoder_attention_mask,
-            inputs_embeds=decoder_inputs_embeds,
-            past_key_values=past_key_values,
-            encoder_hidden_states=hidden_states,
-            encoder_attention_mask=attention_mask,
-            head_mask=decoder_head_mask,
-            cross_attn_head_mask=cross_attn_head_mask,
-            use_cache=use_cache,
-            output_attentions=output_attentions,
-            output_hidden_states=output_hidden_states,
-            return_dict=return_dict,
-        )
-
-        sequence_output = decoder_outputs[0]
-
-        # Set device for model parallelism
-
-        lm_logits = self.lm_head(sequence_output)
-
-        loss = None
-        if lm_labels is not None:
-            loss_fct = CrossEntropyLoss(ignore_index=-100)
-            loss = loss_fct(lm_logits.view(-1, lm_logits.size(-1)), lm_labels.view(-1))
-            # TODO(thom): Add z_loss https://github.com/tensorflow/mesh/blob/fa19d69eafc9a482aff0b59ddd96b025c0cb207d/mesh_tensorflow/layers.py#L666
-
-        if not return_dict:
-            output = (lm_logits,) + decoder_outputs[1:] + encoder_outputs
-            return ((loss,) + output) if loss is not None else output
-
-        return Seq2SeqLMOutput(
-            loss=loss,
-            logits=lm_logits,
-            past_key_values=decoder_outputs.past_key_values,
-            decoder_hidden_states=decoder_outputs.hidden_states,
-            decoder_attentions=decoder_outputs.attentions,
-            cross_attentions=decoder_outputs.cross_attentions,
-            encoder_last_hidden_state=encoder_outputs.last_hidden_state,
-            encoder_hidden_states=encoder_outputs.hidden_states,
-            encoder_attentions=encoder_outputs.attentions,
-        )
+    # def forward(
+    #     self,
+    #     input_ids=None,
+    #     attention_mask=None,
+    #     decoder_input_ids=None,
+    #     decoder_attention_mask=None,
+    #     head_mask=None,
+    #     decoder_head_mask=None,
+    #     cross_attn_head_mask=None,
+    #     encoder_outputs=None,
+    #     past_key_values=None,
+    #     inputs_embeds=None,
+    #     decoder_inputs_embeds=None,
+    #     lm_labels=None,
+    #     use_cache=None,
+    #     output_attentions=None,
+    #     output_hidden_states=None,
+    #     return_dict=None,
+    # ):
+    #     if decoder_input_ids[0, 0] == self.config.decoder_start_token_id:
+    #         decoder = self.dst_decoder
+    #     else:
+    #         decoder = self.decoder
+    #
+    #     # Encode if needed (training, first prediction pass)
+    #     if encoder_outputs is None:
+    #         # Convert encoder inputs in embeddings if needed
+    #         encoder_outputs = self.encoder(
+    #             input_ids=input_ids,
+    #             attention_mask=attention_mask,
+    #             inputs_embeds=inputs_embeds,
+    #             head_mask=head_mask,
+    #             output_attentions=output_attentions,
+    #             output_hidden_states=output_hidden_states,
+    #             return_dict=return_dict,
+    #         )
+    #
+    #     elif return_dict and not isinstance(encoder_outputs, BaseModelOutput):
+    #         encoder_outputs = BaseModelOutput(
+    #             last_hidden_state=encoder_outputs[0],
+    #             hidden_states=encoder_outputs[1] if len(encoder_outputs) > 1 else None,
+    #             attentions=encoder_outputs[2] if len(encoder_outputs) > 2 else None,
+    #         )
+    #
+    #     hidden_states = encoder_outputs[0]
+    #
+    #     if lm_labels is not None and decoder_input_ids is None and decoder_inputs_embeds is None:
+    #         # get decoder inputs from shifting lm labels to the right
+    #         decoder_input_ids = self._shift_right(lm_labels)
+    #
+    #     # Decode
+    #     decoder_outputs = decoder(
+    #         input_ids=decoder_input_ids,
+    #         attention_mask=decoder_attention_mask,
+    #         inputs_embeds=decoder_inputs_embeds,
+    #         past_key_values=past_key_values,
+    #         encoder_hidden_states=hidden_states,
+    #         encoder_attention_mask=attention_mask,
+    #         head_mask=decoder_head_mask,
+    #         cross_attn_head_mask=cross_attn_head_mask,
+    #         use_cache=use_cache,
+    #         output_attentions=output_attentions,
+    #         output_hidden_states=output_hidden_states,
+    #         return_dict=return_dict,
+    #     )
+    #
+    #     sequence_output = decoder_outputs[0]
+    #
+    #     # Set device for model parallelism
+    #
+    #     lm_logits = self.lm_head(sequence_output)
+    #
+    #     loss = None
+    #     if lm_labels is not None:
+    #         loss_fct = CrossEntropyLoss(ignore_index=-100)
+    #         loss = loss_fct(lm_logits.view(-1, lm_logits.size(-1)), lm_labels.view(-1))
+    #         # TODO(thom): Add z_loss https://github.com/tensorflow/mesh/blob/fa19d69eafc9a482aff0b59ddd96b025c0cb207d/mesh_tensorflow/layers.py#L666
+    #
+    #     if not return_dict:
+    #         output = (lm_logits,) + decoder_outputs[1:] + encoder_outputs
+    #         return ((loss,) + output) if loss is not None else output
+    #
+    #     return Seq2SeqLMOutput(
+    #         loss=loss,
+    #         logits=lm_logits,
+    #         past_key_values=decoder_outputs.past_key_values,
+    #         decoder_hidden_states=decoder_outputs.hidden_states,
+    #         decoder_attentions=decoder_outputs.attentions,
+    #         cross_attentions=decoder_outputs.cross_attentions,
+    #         encoder_last_hidden_state=encoder_outputs.last_hidden_state,
+    #         encoder_hidden_states=encoder_outputs.hidden_states,
+    #         encoder_attentions=encoder_outputs.attentions,
+    #     )
 
     def inference(
             self,
